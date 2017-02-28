f8ed8a80af6b07047bef097bc27a51fbc5f9db85
elghobaty <elghobaty@gmail.com>
2017-02-28 08:16:33 +0200

    Diplay error message on error 400 (Bad request)

--- src/app/shared/error-handler/error-handler.interceptor.js
+++ src/app/shared/error-handler/error-handler.interceptor.js
@@ -5,48 +5,53 @@ function ErrorHandlingInterceptor(AppConstants, $injector, toaster, FlashBag, Jw
 
     let responseError =
 
-            rejection => {
+        rejection => {
 
-                if (!rejection || !rejection.config || !_isApiRoute(rejection.config.url)) {
-                    return $q.reject(rejection);
-                }
+            if (!rejection || !rejection.config || !_isApiRoute(rejection.config.url)) {
+                return $q.reject(rejection);
+            }
 
-                var defer = $q.defer();
+            var defer = $q.defer();
 
-                // Not found
-                if (rejection.status == 404) {
-                    $injector.get('$state').go('app.404');
-                }
+            // Not found
+            if (rejection.status == 404) {
+                $injector.get('$state').go('app.404');
+            }
 
-                // Server Error
-                if (rejection.status == 500) {
-                    $injector.get('$state').go('app.500');
-                }
+            // Server Error
+            if (rejection.status == 500) {
+                $injector.get('$state').go('app.500');
+            }
 
-                // Validation Error
-                if (rejection.status == 422) {
-                    for (let errorKey in rejection.data.errors) {
-                        for (let error of rejection.data.errors[errorKey]) {
-                            toaster.pop('error', "Validation Error", error);
-                            break;
-                        }
+            // Validation Error
+            if (rejection.status == 422) {
+                for (let errorKey in rejection.data.errors) {
+                    for (let error of rejection.data.errors[errorKey]) {
+                        toaster.pop('error', "Validation Error", error);
                         break;
                     }
+                    break;
                 }
+            }
 
-                // Unauthorized.
-                if (rejection.status == 401) {
-                    FlashBag.warning("Please login to proceed!");
-                    JwtService.destroy();
-                    $injector.get('$state').go('app.login');
-                }
+            // Bad request error
+            if (rejection.status == 400) {
+                toaster.pop('error', "Error!", rejection.data.message);
+            }
 
-                defer.reject(rejection);
-                return defer.promise;
-            };
+            // Unauthorized.
+            if (rejection.status == 401) {
+                FlashBag.warning("Please login to proceed!");
+                JwtService.destroy();
+                $injector.get('$state').go('app.login');
+            }
+
+            defer.reject(rejection);
+            return defer.promise;
+        };
 
 
-    return { responseError };
+    return {responseError};
 }
 
 
