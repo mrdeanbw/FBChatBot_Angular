5e887620caa1d38a690da2789e232217f224275d
elghobaty <elghobaty@gmail.com>
2017-03-21 20:14:04 +0200

    Helper function to convert array to table (row/columns)

--- src/app/shared/helpers/helpers.service.js
+++ src/app/shared/helpers/helpers.service.js
@@ -1,13 +1,13 @@
 class AppHelpers {
-    
+
     deleteFromArray(array, item) {
         let index = array.indexOf(item);
         if (index != -1) {
             array.splice(index, 1);
         }
     };
-    
-    insertIntoArray(array, index, item){
+
+    insertIntoArray(array, index, item) {
         array.splice(index, 0, item);
     }
 
@@ -17,6 +17,23 @@ class AppHelpers {
         return array;
     };
 
+    tablize(arr, colCount = 3) {
+        let ret = [];
+        let row = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (i % colCount == 0) {
+                if (row.length) {
+                    ret.push(row);
+                }
+                row = [];
+            }
+            row.push(arr[i]);
+        }
+        if (row.length) {
+            ret.push(row);
+        }
+        return ret;
+    }
 }
 
 export default AppHelpers;
